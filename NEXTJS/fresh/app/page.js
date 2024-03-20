37e7bfc09754e2408106995a18613906e3dae9be
LeeYongSeok13 <minamiharuka333@gmail.com>
2024-03-20 09:45:28 +0900

    2024 03 20 커밋

---
+++ NEXTJS/fresh/app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  let name = 'park'
+  let link = 'http://google.com'
+  return (
+    <div>
+      <div>
+        <h4 className="title">애플후레시</h4>
+        <p className="title-sub">by dev { name }</p>
+        <a href={ link }>링크</a>
+      </div>
+    </div>
+  );
+}
